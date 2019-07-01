66f8a7c4eec0cbdfb6b7c96a786de2599be93a10
Josh Landron <JoshLandron@gmail.com>
2019-07-01 13:35:39 -0700

    Update PlayState.lua

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -8,31 +8,31 @@ end
 function PlayState:update(dt)
     -- check if paused and allow paused
     if self.paused then
-        if love.keyboard.wasPressed('space') then
+        if love.keyboard.wasPressed("space") then
             self.paused = false
-            gSounds['pause']:play()
+            gSounds["pause"]:play()
         else
             return
         end
-    elseif love.keyboard.wasPressed('space') then
+    elseif love.keyboard.wasPressed("space") then
         self.paused = true
-        gSounds['pause']:play()
+        gSounds["pause"]:play()
         return
     end
 
     self.paddle:update(dt)
 
-    if love.keyboard.wasPressed('escape') then
+    if love.keyboard.wasPressed("escape") then
         love.event.quit()
     end
+end
 
-    function PlayState:render()
-        -- body
-        self.paddle:render()
+function PlayState:render()
+    -- body
+    self.paddle:render()
 
-        if self.paused then
-            love.graphics.setFont(gFonts['large'])
-            love.graphics.printf('PAUSED', 0, VIRTUAL_HEIGHT / 2 - 14, VIRTUAL_WIDTH / 2 - 14)
-        end
+    if self.paused then
+        love.graphics.setFont(gFonts["large"])
+        love.graphics.printf("PAUSED", 0, VIRTUAL_HEIGHT / 2 - 14, VIRTUAL_WIDTH / 2 - 14)
     end
 end
